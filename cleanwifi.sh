6727001bdd6f10f5d89e65c7a2e62164a580cdf1
Dave Landers <dave@srednal.com>
2015-03-19 12:29:35 -0600

    Yosemite launchd changed.  adjust throttle and sleep to make this appear to be daemon-like

--- cleanwifi.sh
+++ cleanwifi.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 #
-# Run from /Library/LaunchAgents/com.srednal.cleanwifi.plist
+# Run from /Library/LaunchDaemons/com.srednal.cleanwifi.plist
 #
 # I do not want to auto-connect to these networks, so remove them from the preferred list
 #
 networksetup -removepreferredwirelessnetwork en1 "EMC Guest Cafe"
 networksetup -removepreferredwirelessnetwork en1 "Corp-W1F1"
 
+# launchd expects this to keep running, but it doesnt.
+# so hang around at least as long as the ThrottleInterval so it won't try
+# to respawn it
+sleep 2
 
